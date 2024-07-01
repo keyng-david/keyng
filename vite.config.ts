d653b11bf43b040bffdf5007f417afe77daf51ae
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-01 12:47:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/final-version/'
+  base: '/keyng/' // replace 'keyng' with your repository name
 })
